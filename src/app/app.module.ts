dea240b1463cab5b38fa46c8b248fcdd98094fba
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2023-09-10 19:06:40 -0300

    add updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,12 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
 // import { environment } from 'src/environments/environment';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA6dgJcg0Sj_myZNJEsCD2vsvrYWc0WjSA",
-  authDomain: "crud-firebase-d11c1.firebaseapp.com",
-  projectId: "crud-firebase-d11c1",
-  storageBucket: "crud-firebase-d11c1.appspot.com",
-  messagingSenderId: "905340005490",
-  appId: "1:905340005490:web:90773ad89deee39b4666eb"
+  apiKey: "AIzaSyBmCUD7IbFcDsKX4xdfqRfWQseI7U7T1t8",
+  authDomain: "crud-firebase-474f7.firebaseapp.com",
+  projectId: "crud-firebase-474f7",
+  storageBucket: "crud-firebase-474f7.appspot.com",
+  messagingSenderId: "123743231531",
+  appId: "1:123743231531:web:49cc233654a2faa6a5d46e"
 };
 
 @NgModule({
