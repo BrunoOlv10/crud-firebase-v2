751a816938e27341e8d84272af48652a73896bcb
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2023-09-10 18:32:50 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,21 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AngularFireModule } from '@angular/fire/compat'
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
-import { environment } from 'src/environments/environment';
+// import { environment } from 'src/environments/environment';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6dgJcg0Sj_myZNJEsCD2vsvrYWc0WjSA",
+  authDomain: "crud-firebase-d11c1.firebaseapp.com",
+  projectId: "crud-firebase-d11c1",
+  storageBucket: "crud-firebase-d11c1.appspot.com",
+  messagingSenderId: "905340005490",
+  appId: "1:905340005490:web:90773ad89deee39b4666eb"
+};
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
-  AngularFireModule.initializeApp(environment.firebaseConfig), AngularFirestoreModule],
+  AngularFireModule.initializeApp(firebaseConfig), AngularFirestoreModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
